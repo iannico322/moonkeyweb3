2bc1e835be1369bba73077d1b4163f4e7fefaf51
iannico322 <iannicocaulin@gmail.com>
2024-10-12 11:52:33 +0800

    ok

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,20 @@
 let button = document.querySelector(".button")
 let monkey = document.querySelector(".monkey")
+let text = document.querySelector(".text")
+
 
 let change = false
 button.addEventListener('click',()=>{
 
-    change?
-
-    monkey.src = "./images/lunar-radiance-clear-background-moon-free-png.webp":
-    monkey.src = "./images/1720229730763.jpg"
+    if (!change) {
+        text.textContent = "Moon"
+        monkey.src = "./images/lunar-radiance-clear-background-moon-free-png.webp"
+        
+    } else {
+        text.textContent = "Nico"
+        monkey.src = "./images/1720229730763.jpg"
+    }
+    
 
     change = !change
 
