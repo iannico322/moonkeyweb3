b25be06aa8646e93e1c76ba593833fde89cfa49c
iannico322 <iannicocaulin@gmail.com>
2024-10-12 11:25:18 +0800

    ok

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ button.addEventListener('click',()=>{
     change?
 
     monkey.src = "./images/lunar-radiance-clear-background-moon-free-png.webp":
-    monkey.src = "./images/Monkey-Selfie.webp"
+    monkey.src = "./images/1720229730763.jpg"
 
     change = !change
 
